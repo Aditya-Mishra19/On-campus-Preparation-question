c466b51ba02ce8a4c3f2076741d6c3ce41437861
Aditya Mishra <74649588+Aditya-Mishra19@users.noreply.github.com>
2024-04-29 21:38:27 +0530

    Update SecondLargest&Min.py
    updated code

--- SecondLargest&Min.py
+++ SecondLargest&Min.py
@@ -1,23 +1,29 @@
 def getSecondLargest(arr):
+    if len(arr) < 2:
+        return -1
     largest, secondLargest = float('-inf'), float('-inf')
     for num in arr:
         if num > largest:
+            secondLargest = largest
             largest = num
-        elif num > secondLargest and num < largest:
+        elif num > secondLargest:
             secondLargest = num 
     return secondLargest if secondLargest != float('-inf') else -1
 
 def getSecondMin(arr):
+    if len(arr) < 2:
+        return -1
     minimum, secondMin = float('inf'), float('inf')
     for num in arr:
         if num < minimum:
+            secondMin = minimum
             minimum = num
-        elif num < secondMin and num > minimum:
+        elif num < secondMin:
             secondMin = num 
     return secondMin if secondMin != float('inf') else -1
 
 
-arr = [1]
+arr = [1,2,3,4,5,6,7]
 secondLargest = getSecondLargest(arr)
 secondMin = getSecondMin(arr)
 print("Second Largest: ", secondLargest, "\nSecond Minimum: ", secondMin)
